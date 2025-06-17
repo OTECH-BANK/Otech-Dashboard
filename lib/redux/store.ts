1351fb7abc1db3be1540f6eac5cc053b1576fc48
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-06-17 12:57:30 +0100

    refresh

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,35 +1,38 @@
 // src/lib/redux/store.ts
 import { configureStore } from "@reduxjs/toolkit"
-import { api } from "./api"
-import { employeeApi } from "./employeeApi" // Import the new API
-import { feesApi } from "./feesApi"
+
+// Feature imports
 import authReducer from "./features/auth/authSlice"
-import { customerTypeApi } from "./customerTypeApi"
+
+// API imports (alphabetical order might be preferred)
+import { api } from "./api"
 import { customerApi } from "./customerApi"
-import { transactionApi } from "./transactionApi"
+import { customerTypeApi } from "./customerTypeApi"
+import { employeeApi } from "./employeeApi"
+import { feesApi } from "./feesApi"
 import { otectplusApi } from "./otechplusApi"
+import { transactionApi } from "./transactionApi"
 
 export const store = configureStore({
   reducer: {
-    [api.reducerPath]: api.reducer,
-    [employeeApi.reducerPath]: employeeApi.reducer, // Add the employee API reducer
-    [feesApi.reducerPath]: feesApi.reducer,
-    [customerTypeApi.reducerPath]: customerTypeApi.reducer,
-    [customerApi.reducerPath]: customerApi.reducer,
-    [transactionApi.reducerPath]: transactionApi.reducer,
-    [otectplusApi.reducerPath]: otectplusApi.reducer,
-
     auth: authReducer,
+    [api.reducerPath]: api.reducer,
+    [customerApi.reducerPath]: customerApi.reducer,
+    [customerTypeApi.reducerPath]: customerTypeApi.reducer,
+    [employeeApi.reducerPath]: employeeApi.reducer,
+    [feesApi.reducerPath]: feesApi.reducer,
+    [otectplusApi.reducerPath]: otectplusApi.reducer,
+    [transactionApi.reducerPath]: transactionApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(api.middleware)
+      .concat(customerApi.middleware)
+      .concat(customerTypeApi.middleware)
       .concat(employeeApi.middleware)
       .concat(feesApi.middleware)
-      .concat(customerTypeApi.middleware)
-      .concat(customerApi.middleware)
-      .concat(transactionApi.middleware)
-      .concat(otectplusApi.middleware),
+      .concat(otectplusApi.middleware)
+      .concat(transactionApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
